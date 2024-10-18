f81da2b284fa77ca9700cd68fbcc543a7b707624
User <125206457+ghostuser-bug@users.noreply.github.com>
2024-10-18 13:38:49 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,72 +1,156 @@
 import os
-try:
- import requests
- from time import sleep
- from configparser import ConfigParser
- from os import system, name
- from threading import Thread, active_count
- from re import search, compile
-except:
- os.system('pip install requests')
- os.system('pip install configparser')
-THREADS = 500
+import requests
+from time import sleep
+from configparser import ConfigParser
+from os import system, name
+from threading import Thread, active_count
+import csv
+import phonenumbers
+from phonenumbers import PhoneNumber, PhoneNumberFormat
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+from bs4 import BeautifulSoup
+import random
+from emailtools import generate
+
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value, SoftwareName.OPERA.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
+
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=1200)
+
+THREADS = 600
 PROXIES_TYPES = ('http', 'socks4', 'socks5')
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-REGEX = compile(r"(?:^|\D)?(("+ r"(?:[1-9]|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"):" + (r"(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}"
-                + r"|65[0-4]\d{2}|655[0-2]\d|6553[0-5])")
-                + r")(?:\D|$)")
 
 errors = open('errors.txt', 'a+')
-cfg = ConfigParser(interpolation=None)
-cfg.read("config.ini", encoding="utf-8")
 
-http, socks4, socks5 = '', '', ''
-try: socks4, socks5, http = cfg["SOCKS4"], cfg["SOCKS5"], cfg["HTTP"]
-except KeyError: print(' [ OUTPUT ] Error | config.ini file not found pls make sure the file is in the same dir!');sleep(3);exit()
-
-http_proxies, socks4_proxies, socks5_proxies = [], [], []
 time_out = 15
+success_count = 0
+error_count = 0
+username = ""
 
-def save_proxies(proxies, proxy_type):
-    with open(f"{proxy_type}_proxies.txt", 'w') as file:
-        for proxy in proxies:
-            file.write(proxy + '\n')
+def generate_random_phone_number():
+    while True:
+        country_code = "+{}".format(random.randint(1, 999))
+        national_number = str(random.randint(1000000000, 9999999999))
+        phone_number_str = country_code + national_number
+        try:
+            phone_number = phonenumbers.parse(phone_number_str)
+            if phonenumbers.is_valid_number(phone_number):
+                return phonenumbers.format_number(phone_number, PhoneNumberFormat.E164)
+        except phonenumbers.phonenumberutil.NumberParseException:
+            continue
 
-def scrap(sources, _proxy_type):
-    proxies = []
-    for source in sources:
-        if source:
-            try:
-                response = requests.get(source, timeout=time_out)
-                if tuple(REGEX.finditer(response.text)):
-                    for proxy in tuple(REGEX.finditer(response.text)):
-                        proxies.append(proxy.group(1))
-            except Exception as e:
-                errors.write(f'{e}\n')
+def get_random_line(filename, username):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        line = random.choice(lines).strip()
+        return line.replace('{username}', username)
 
-           if _proxy_type == 'socks4':
-                save_proxies(proxies, 'socks4')
-            elif _proxy_type == 'socks5':
-                save_proxies(proxies, 'socks5')
-                elif _proxy_type == 'http':
-                save_proxies(proxies, 'http')
-
-def start_scrap():
-    threads = []
-    for i in (http_proxies, socks4_proxies, socks5_proxies): i.clear()
-    for i in (((socks4.get("Sources").splitlines(), 'socks4'), (socks5.get("Sources").splitlines(), 'socks5')(http.get("Sources").splitlines(), 'http')):
-        thread = Thread(target=scrap, args=(i[0], i[1]))
-        threads.append(thread)
-        thread.start()
-    for t in threads: t.join()
+def control(proxy, proxy_type, username):
     
+    global success_count
+    global error_count
+    
+    USER_AGENT = user_agent_rotator.get_random_user_agent()
+    url = 'https://telegram.org/support'
+    try:
+        response = requests.get(url, proxies={'http': f'{proxy_type}://{proxy}', 'https': f'{proxy_type}://{proxy}'}, timeout=time_out)
+    except AttributeError:
+        error_count += 1
+        pass
+    except Exception as e:
+        error_count += 1
+        return errors.write(f'{e}\n')
+        
+    cookies = response.cookies
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    form = soup.find('form', action="/support")
+
+    if not form:
+        print("Form not found on the page.")
+        exit()
+
+    message_input = form.find('textarea', id='support_problem')
+    email_input = form.find('input', id='support_email')
+    phone_input = form.find('input', id='support_phone')
+
+    message = get_random_line('message.txt', username)
+    email = generate('gmail,hotmail')
+    phone = generate_random_phone_number()
+
+    if message_input:
+        message_input['value'] = message
+    if email_input:
+        email_input['value'] = email
+    if phone_input:
+        phone_input['value'] = phone
+
+    data = {input['name']: input.get('value', '') for input in form.find_all(['input', 'textarea'])}
+
+    hidden_inputs = form.find_all('input', type='hidden')
+    for hidden_input in hidden_inputs:
+        data[hidden_input['name']] = hidden_input['value']
+
+    headers = {
+        'User-Agent': USER_AGENT
+    }
+    try:
+        response = requests.post(url, data=data, cookies=cookies, headers=headers)
+
+        if response.status_code == 200:
+            print(f"Report Successful with Email: {email}, Phone Number: {phone} and Message: {message} using Proxy: {proxy, proxy_type}\n")
+            success_count += 1
+        else:
+            error_count += 1
+            pass
+            
+    except AttributeError:
+        error_count += 1
+        pass
+    except requests.exceptions.RequestException:
+        error_count += 1
+        pass
+    except Exception as e:
+        error_count += 1
+        return errors.write(f'{e}\n')
+    
+def get_views_from_saved_proxies(proxy_type, proxies, username):
+    for proxy in proxies:
+        control(proxy.strip(), proxy_type, username)
+
 def start_view():
-    c, threads = 0, []
-    start_scrap()
+
+    while True:
+            threads = []
+            for proxy_type in PROXIES_TYPES:
+                with open(f"{proxy_type}_proxies.txt", 'r') as file:
+                    proxies = file.readlines()
+                chunked_proxies = [proxies[i:i + 70] for i in range(0, len(proxies), 70)]
+                for chunk in chunked_proxies:
+                    thread = Thread(target=get_views_from_saved_proxies, args=(proxy_type, chunk, username))
+                    threads.append(thread)
+                    thread.start()
+            for t in threads:
+                t.join()
+
+        
+E = '\033[1;31m'
+B = '\033[2;36m'
+G = '\033[1;32m'
+S = '\033[1;33m'
+
+def check_views():
+
+    global success_count
+    global error_count
     
+    while True:
+        print(f'{G}[ THREADS ]: {B}{active_count()} ⇝⇝⇝⇝ \n{G}[ SUCCESSFULL REPORT ]: {S}{success_count}\n{G}[ FAILED ]: {E}{error_count}\n')
+        
+        sleep(4)
+
+username = input("Pls enter username or link of Person, Channel or Group:")
 
 Thread(target=start_view).start()
+Thread(target=check_views).start()
